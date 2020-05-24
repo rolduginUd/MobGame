64f2da9d22be82b56b352c012f04b6efd13e5dc1
Yevhenii Rolduhin <51542006+rolduginUd@users.noreply.github.com>
2020-05-24 14:54:27 +0300

    comit

--- HillClimb/script.js
+++ HillClimb/script.js
@@ -1,22 +1,16 @@
 let canvas = document.getElementById('canvas'); 
 let ctx = canvas.getContext("2d");
-if(window.matchMedia('(max-width: 768px)').matches){
-    function inRad(num) {
-        return num * Math.PI / 180;
-    }
-    canvas.width = document.documentElement.clientWidth; // розміри "вікна"
-    canvas.height = document.documentElement.clientHeight;
-    ctx.rotate(inRad(0));
-} else {
-    canvas.width = document.documentElement.clientWidth; // розміри "вікна"
-    canvas.height = document.documentElement.clientHeight;
-  }
+    window.addEventListener("orientationchange", function() {
+        canvas.width = document.documentElement.clientWidth; 
+        canvas.height = document.documentElement.clientHeight;
+    }, false);
+   
 
 document.body.appendChild(canvas); 
 
 let perm = [];
 while(perm.length < 255) {
-    while(perm.includes(val = Math.floor(Math.random()*255)));
+    while(perm.includes(val = Math.floor(Math.random()*355)));
     perm.push(val);
 }
 
@@ -28,7 +22,7 @@ let noise = x => {
 }
 
 let car = new function () { // гравець і його параметри
-    this.x = canvas.width / 2;
+    this.x = canvas.width/2;
     this.y = 0;
     this.speedY = 0;
     this.speedX = 0;
@@ -37,12 +31,13 @@ let car = new function () { // гравець і його параметри
     this.img = new Image();
     this.img.src = "img/red_car.png"
     this.draw = function () {
-        let p1 = canvas.height - noise(time + this.x) * 0.47;    // оце тоже впливає 
-        let p2 = canvas.height - noise(time + 5 + this.x) * 0.47;//                на положення картінки
+        let p1 = canvas.height - noise(time + this.x) * 0.30;    // оце тоже впливає 
+        let p2 = canvas.height - noise(time + 5 + this.x) * 0.30;//                на положення картінки
 
         let onGround = false;
         if(p1 - 15 > this.y) {
             this.speedY += 0.1;
+    
         }else {
             this.speedY -= this.y - (p1 - 15);
             this.y = p1 - 15;
@@ -59,32 +54,37 @@ let car = new function () { // гравець і його параметри
         if(onGround) { // чи знаходиться гравець на землі
             this.rotate -=(this.rotate - angle) * 0.5;
             this.speedX = this.speedX - (angle - this.rotate);
+
         } 
         this.speedX += (controller.a - controller.d) * 0.05;
         this.rotate -= this.speedX * 0.1;
-        if(this.rotate > Math.PI) this.rotate = -Math.PI;
-        if(this.rotate < -Math.PI) this.rotate = Math.PI;
+        if(this.rotate > Math.PI){
+            this.rotate = -Math.PI;
+        } 
+        if(this.rotate < -Math.PI)
+        {
+            this.rotate = Math.PI;
+        } 
         
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.rotate);
-        ctx.drawImage(this.img, 0, 0, 80, 50);
+        ctx.drawImage(this.img, -40, -25, 80, 50);
         ctx.restore();
     }
 }
-
 
 let time = 0; // час для генерації дороги
 let speed = 0;
 let controller = {
     w: 0,
-    s: 0, 
+    s: 0,
     a: 0,
     d: 0
 };
 function loop() {
     speed -= (speed - (controller.w - controller.s)) * 0.01;
-    time += 5 * speed;
+    time += 15 * speed;
     ctx.fillStyle = "#19f";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
