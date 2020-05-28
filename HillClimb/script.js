3483585a03a40fb5cf1aea272e15fde32efbd9f9
Yevhenii Rolduhin <51542006+rolduginUd@users.noreply.github.com>
2020-05-28 18:10:00 +0300

    comit

--- HillClimb/script.js
+++ HillClimb/script.js
@@ -1,10 +1,11 @@
 let canvas = document.getElementById('canvas'); 
 let ctx = canvas.getContext("2d");
+let money = 0;
 
 // $('#myModal').modal(options)
 
-stopBtn = document.querySelector('.stop');
-runBtn = document.querySelector('.run');
+let stopBtn = document.querySelector('.stop');
+let runBtn = document.querySelector('.run');
 
 setInterval(() => {
     checkOrientation();
@@ -63,13 +64,7 @@ let car = new function () { // гравець і його параметри
         this.y += this.speedY;
 
         if(onGround && Math.abs(this.rotate) > 1.6) { // смерть пожила
-            alert("Шо лох, наєбнувся?)");
-            this.x = canvas.width/2;
-            this.y = 0;
-            this.speedY = 0;
-            this.speedX = 0;
-            this.rotate = 0;
-
+            // location.reload();
         }
 
         // контроллер для моб.
@@ -107,14 +102,16 @@ let car = new function () { // гравець і його параметри
 
         } 
         this.speedX += (controller.a - controller.d) * 0.05;
+        
         this.rotate -= this.speedX * 0.1;
         if(this.rotate > Math.PI){
             this.rotate = -Math.PI;
-            console.log('+10');
+            money+= 50;
         } 
         if(this.rotate < -Math.PI)
         {
             this.rotate = Math.PI;
+            money+= 50;
         } 
         ctx.save();
         ctx.translate(this.x, this.y);
@@ -131,28 +128,52 @@ let car = new function () { // гравець і його параметри
 
 let time = 0; 
 let speed = 0;
-const controller = {
+
+
+const controller = { // контроллер
     w: 0,
     s: 0,
     a: 0,
     d: 0
 };
+onkeydown = somekey => controller[somekey.key] = 1;
+onkeyup = somekey => controller[somekey.key] = 0;
+ 
 
 //покупка
 document.querySelector('.cart1').onclick = () => {
-    car.img.src = "img/lvl1.png";
+    if(money >= 100) {
+        car.img.src = "img/lvl1.png";
+        $('#staticBackdrop').modal('hide');
+        money = money - 100;
+    }else {
+        alert('У вас не достаточно денег')
+    }
 }
 document.querySelector('.cart2').onclick = () => {
-    car.img.src = "img/lvl2.png";
+    if(money >= 200) {
+        car.img.src = "img/lvl2.png";
+        $('#staticBackdrop').modal('hide');
+        money = money - 200;
+    }else {
+        alert('У вас не достаточно денег')
+    }
 }
 document.querySelector('.cart3').onclick = () => {
-    car.img.src = "img/lvl3.png";
+    if(money >= 300) {
+        car.img.src = "img/lvl3.png";
+        $('#staticBackdrop').modal('hide');
+        money = money - 300;
+    }else {
+        alert('У вас не достаточно денег')
+    }
 }
 
 
 function loop() {
     speed -= (speed - (controller.w - controller.s)) * 0.01;
     time += 5 * speed;
+    document.querySelector('.money').innerHTML = money;
     ctx.fillStyle = "#19f";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -169,8 +190,5 @@ function loop() {
 
     requestAnimationFrame(loop);
 }
-
-onkeydown = somekey => controller[somekey.key] = 1;
-onkeyup = somekey => controller[somekey.key] = 0;
 
 loop();
